46cae8929405c216965bc446954002e62e91965a
adam <a2e56b85@gmail.com>
2025-08-04 19:21:15 +0800

    l

--- config/database.php
+++ config/database.php
@@ -84,7 +84,6 @@ return [
 
 'pgsql' => [
     'driver' => 'pgsql',
-    'url' => env('DATABASE_URL'),
     'host' => env('DB_HOST', '127.0.0.1'),
     'port' => env('DB_PORT', '5432'),
     'database' => env('DB_DATABASE', 'forge'),
@@ -92,9 +91,9 @@ return [
     'password' => env('DB_PASSWORD', ''),
     'charset' => 'utf8',
     'prefix' => '',
-    'prefix_indexes' => true,
     'schema' => 'public',
-    'sslmode' => 'require',
+    'sslmode' => 'prefer',
+    'options' => extension_loaded('pdo_pgsql') ? [] : null,
 ],
 
 
